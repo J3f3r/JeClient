f7b782122b4e4a4087740e4ceb7e2177a58f6bf7
jeferson <jsmartins959@gmail.com>
2025-11-26 23:18:13 -0300

    Adiciona POST new Client com 201 created e PUT Client

--- src/main/java/com/jeferson/jeclient/controllers/ClientController.java
+++ src/main/java/com/jeferson/jeclient/controllers/ClientController.java
@@ -4,15 +4,15 @@ import com.jeferson.jeclient.dto.ClientDTO;
 import com.jeferson.jeclient.entities.Client;
 import com.jeferson.jeclient.repositories.ClientRepository;
 import com.jeferson.jeclient.service.ClientService;
+import jakarta.servlet.Servlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,13 +24,28 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping(value = "/{id}")
-    public ClientDTO findById(@PathVariable Long id){
-        return service.findById(id);
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping
     public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
         Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+                buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+        dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/jeferson/jeclient/service/ClientService.java
+++ src/main/java/com/jeferson/jeclient/service/ClientService.java
@@ -26,9 +26,39 @@ public class ClientService {
         return dto;
     }
 
-    @Transactional(readOnly = true)
+    @Transactional(readOnly = true)// faz a paginação da lista de clientes e permite organizar clientes por pagina e ordenação por nome
     public Page<ClientDTO> findAll(Pageable pageable){
         Page<Client> result = repository.findAll(pageable);
         return  result.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        entity = repository.save(entity);
+
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        Client entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client entity) {// esse método é chamado dentro do update para usar esses campos
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 }
